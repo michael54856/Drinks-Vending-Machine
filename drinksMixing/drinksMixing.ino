2ad8ac1636a01d07eaf251a61c2554b22510aefd
michael548562 <michael548562@gmail.com>
2022-06-13 23:27:33 +0800

    update ppt and program

--- drinksMixing/drinksMixing.ino
+++ drinksMixing/drinksMixing.ino
@@ -1,4 +1,6 @@
 #include <Servo.h>
+#include <MySQL_Connection.h>
+#include <MySQL_Cursor.h>
 int out = 2;
 int s0 = 3;
 int s1 = 4;
@@ -10,6 +12,10 @@ int GreenColor = 0;
 int BlueColor = 0;
 
 int colorArr[8][3] = {{7,4,10},{15,10,6},{14,11,11},{8,9,6}, {5,6,8},{6,12,10}, {16,17,13}, {12,10,11}};
+
+int RedDrinks = 100; //紅色飲料剩餘量
+int GreenDrinks = 100; //綠色飲料剩餘量
+int BlueDrinks = 100; //藍色飲料剩餘量
 
 
 Servo myservo;  // 建立SERVO物件
@@ -28,6 +34,7 @@ void ScanColor()
 
 void giveDrinks(int n)
 {
+    //500ms 是用來充滿水管
     //9 -> r
     //10 -> g
     //13 -> b
@@ -36,57 +43,70 @@ void giveDrinks(int n)
         digitalWrite(9,LOW); 
         delay(2000);
         digitalWrite(9,HIGH); 
+        RedDrinks -= 15;
     }
     else if(n == 1)//藍
     {
         digitalWrite(13,LOW); 
         delay(2000);
         digitalWrite(13,HIGH); 
+        BlueDrinks -= 15;
     }
     else if(n == 2)//綠
     {
         digitalWrite(10,LOW); 
         delay(2000);
         digitalWrite(10,HIGH); 
+        GreenDrinks -= 15;
     }
     else if(n == 3)//紫
     {
         digitalWrite(9,LOW); 
         digitalWrite(13,LOW); 
-        delay(1500);
+        delay(1300);
         digitalWrite(9,HIGH); 
         digitalWrite(13,HIGH); 
+        RedDrinks -= 8;
+        BlueDrinks -= 8;
     }
     else if(n == 4)//天藍
     {
         digitalWrite(10,LOW); 
         digitalWrite(13,LOW); 
-        delay(1500);
+        delay(1300);
         digitalWrite(10,HIGH); 
         digitalWrite(13,HIGH); 
+        GreenDrinks -= 8;
+        BlueDrinks -= 8;
     }
     else if(n == 5)//橘
     {
         digitalWrite(9,LOW); 
         digitalWrite(10,LOW); 
-        delay(1500);
+        delay(1300);
         digitalWrite(9,HIGH); 
         digitalWrite(10,HIGH); 
+        RedDrinks -= 8;
+        GreenDrinks -= 8;
     }
     else if(n == 6)//黑
     {
         digitalWrite(9,LOW); 
         digitalWrite(10,LOW); 
         digitalWrite(13,LOW); 
-        delay(1500);
+        delay(1000);
         digitalWrite(9,HIGH); 
         digitalWrite(10,HIGH); 
         digitalWrite(13,HIGH); 
+        RedDrinks -= 5;
+        GreenDrinks -= 5;
+         BlueDrinks -= 5;
     }
     else//預設顏色,沒有投入代幣
     {
         
     }
+
     
 }
 
@@ -123,9 +143,11 @@ void loop() {
      if(comingValue == '1')//藍芽傳東西過來
      {
         ScanColor();
+        /*
         Serial.print("Red: "); Serial.print(RedColor); Serial.print("\n");
         Serial.print("Green: "); Serial.print(GreenColor); Serial.print("\n");
         Serial.print("Blue : "); Serial.print(BlueColor); Serial.print("\n");
+        */
 
         //旋轉伺服馬達讓代幣掉下去
         myservo.write(180);  
@@ -148,7 +170,11 @@ void loop() {
             }
         }
         giveDrinks(chooseColor);
-        
+
+        String s1 = String(RedDrinks) + "," + String(GreenDrinks) + "," + String(BlueDrinks);
+        char Buffer[100];
+        s1.toCharArray(Buffer, 100);
+        Serial.write(Buffer);
      }
   }
 }
